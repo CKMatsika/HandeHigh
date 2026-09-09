298ce502accb6af20f8bf669c162f4725d138ac5
CKMatsika <clemie90@gmail.com>
2026-09-09 23:40:54 +0200

    Assign Hande High School to super-admin in seeder

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -12,14 +12,33 @@ class SuperAdminSeeder extends Seeder
      */
     public function run(): void
     {
+        $school = \App\Models\School::firstOrCreate(
+            ['code' => 'HANDE'],
+            [
+                'name' => 'Hande High School',
+                'email' => 'admin@handehigh.test',
+                'phone' => null,
+                'address' => null,
+                'timezone' => 'Africa/Harare',
+                'currency' => 'USD',
+                'setup_completed' => true,
+            ]
+        );
+
         $superAdmin = \App\Models\User::firstOrCreate(
             ['email' => 'superadmin@handehigh.test'],
             [
                 'name' => 'Super Admin',
                 'password' => bcrypt('password'), // Or whatever password you'd like
                 'email_verified_at' => now(),
+                'school_id' => $school->id,
             ]
         );
+
+        // If the super admin already exists, update their school_id
+        if (!$superAdmin->school_id) {
+            $superAdmin->update(['school_id' => $school->id]);
+        }
 
         $role = \Spatie\Permission\Models\Role::firstOrCreate(['name' => 'super-admin', 'guard_name' => 'web']);
 
